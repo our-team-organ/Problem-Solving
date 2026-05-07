5c1116931edf8f0fa30dd18a8e212b14ed54281f
Almas Sayem <mdassayem0@gmail.com>
2026-05-07 07:50:22 +0600

    updated

---
+++ Sayem/task3.py
@@ -0,0 +1,15 @@
+#31. Prime Number Check. 
+#32. Print All Prime Numbers (1 to N).
+#33. GCD (Greatest Common Divisor). 
+#34. LCM (Least Common Multiple). 
+#35. Perfect Number. 
+#36. Strong Number. 
+#37. Binary to Decimal. 
+#38. Decimal to Binary.
+#39. Menu Driven Calculator
+#40. Login System (3 Attempts)
+#41. Guessing Game (3 Attempts)
+#42. Digit Frequency
+#43. Second Largest (3 Numbers)
+#44. Sum of Even Numbers (1 to N)
+#45. Pattern Printing ⭐
